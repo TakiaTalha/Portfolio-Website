0030619ea1f5b831bc3d3295477a1476aa01f853
TakiaTalha <ktakiatalha@gmail.com>
2024-01-01 18:49:31 -0500

    add

--- text.js
+++ text.js
@@ -1,5 +1,3 @@
-
-// Define an array of texts to transition
 const texts = ['CS Student', 'ML Developer', 'Website Manager', 'Campus Ambassador'];
 
 let count = 0;
@@ -14,8 +12,21 @@ function changeText() {
         count = (count + 1) % texts.length;
         textElement.textContent = texts[count];
         textElement.style.opacity = 1;
-    }, 1000); // Wait for 1 second before changing text
+    }, 1000);
 }
 
 // Change text every 4 seconds
 setInterval(changeText, 1000);
+
+window.addEventListener('scroll', function() {
+    var element = document.getElementById('animatedBox');
+    var position = element.getBoundingClientRect();
+
+    // Checking if the element is within the viewport
+    if(position.top < window.innerHeight && position.bottom >= 0) {
+        document.querySelector('.top-border').style.width = '100%';
+        document.querySelector('.bottom-border').style.width = '100%';
+        document.querySelector('.left-border').style.height = '100%';
+        document.querySelector('.right-border').style.height = '100%';
+    }
+});
